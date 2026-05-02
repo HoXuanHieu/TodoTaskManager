c7d4a1c1288f12cf9b16caf5a4692ba761dab0e8
HoXuanHieu <hxhieu123@gmail.com>
2026-05-02 23:29:22 +0700

    add logic for task service

--- src/app/service/task-service.ts
+++ src/app/service/task-service.ts
@@ -1,6 +1,13 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+import { Task } from '../models/task.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class TaskService {}
+export class TaskService {
+  tasks = signal<Task[]>([]);
+  
+  addTask(task: string, stastus: string, priority: string) {
+    this.tasks.update((previousState) => [...tasks, task]);
+  }
+}
