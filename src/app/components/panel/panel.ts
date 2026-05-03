f827a4b1168e1b1f5b7c104c7ccdd46609bacf75
HoXuanHieu <hxhieu123@gmail.com>
2026-05-03 14:32:41 +0700

    add heading for each panel

--- src/app/components/panel/panel.ts
+++ src/app/components/panel/panel.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-panel',
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './panel.html',
   styleUrl: './panel.css',
 })
-export class Panel {}
+export class Panel {
+  heading = input.required<string>();
+
+}
