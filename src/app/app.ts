990ebb4111df601d878cb1376e0831080c293845
HoXuanHieu <hxhieu123@gmail.com>
2026-05-03 14:33:01 +0700

    add logic for each task

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TaskForm } from './components/task-form/task-form';
 import { Panel } from './components/panel/panel';
+import { TaskService } from './service/task-service';
 // import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -11,5 +12,12 @@ import { Panel } from './components/panel/panel';
   styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('TodoList-Angular');
+  // protected readonly title = signal('TodoList-Angular');
+  taskservices = inject(TaskService);
+
+  tasks = this.taskservices.tasks;
+
+  todoItems = this.tasks().filter(task => task.status === 'Todo');
+  inProgressItems = this.tasks().filter(task => task.status === 'In Progress');
+  completedItems = this.tasks().filter(task => task.status === 'Done');
 }
