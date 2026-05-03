6a4c59613581a0cd1ce693a398f02969111cde04
HoXuanHieu <hxhieu123@gmail.com>
2026-05-03 15:17:31 +0700

    add panel item component

---
+++ src/app/components/panel-item/panel-item.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-panel-item',
+  imports: [],
+  templateUrl: './panel-item.html',
+  styleUrl: './panel-item.css',
+})
+export class PanelItem {}
